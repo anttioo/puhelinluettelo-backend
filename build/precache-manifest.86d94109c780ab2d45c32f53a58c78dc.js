4a8d38d38243665c15c817d180ca4486c78cc3bf
Antti Ollikkala <antti@huom.io>
2020-02-09 23:07:05 +0200

    added build to git

---
+++ build/precache-manifest.86d94109c780ab2d45c32f53a58c78dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e9b79688540a80b169e6e25c4256b10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "956022dca698d62910d3",
+    "url": "/static/css/main.5c4bac34.chunk.css"
+  },
+  {
+    "revision": "82f9c65a616ba348d652",
+    "url": "/static/js/2.67c43bda.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.67c43bda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "956022dca698d62910d3",
+    "url": "/static/js/main.60693f64.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
