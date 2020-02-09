111cd124456fd95dc67196d5b11821f9b37cc6fb
Antti Ollikkala <antti@huom.io>
2020-02-10 01:43:21 +0200

    mongo.js

---
+++ mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+if ( process.argv.length<3 ) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack2020:${password}@cluster0-vf96p.mongodb.net/fullstack-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if ( process.argv.length === 3 ) {
+    Person.find({}).then( result => {
+        result.forEach( person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+        process.exit(0)
+    })
+}
+
+const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+})
+
+person.save().then(response => {
+    console.log('Person saved!');
+    mongoose.connection.close();
+})
